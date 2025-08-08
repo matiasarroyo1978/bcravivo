c95a53ead6a349b9e811076ae35744c09a4d4493
Matias Arroyo <matiasarroyo684@gmail.com>
2025-08-08 19:38:45 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "La Macro - Datos de la Macroeconomía Argentina",
+    title: "Datos Macro - Datos de la Macroeconomía Argentina",
     description:
       "Central de deudores, inflación, reservas, dólar, tasas, y calculadora de carry trade.",
     creator: "@arroyomatias19",
